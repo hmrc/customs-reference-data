7b0f3f7582a4997840ac1a179d30a04abc864c91
Amaal Ali <9220299+amaalali@users.noreply.github.com>
2020-08-18 15:45:35 +0100

    Add first test for controller.

---
+++ test/base/SpecBase.scala
@@ -0,0 +1,32 @@
+/*
+ * Copyright 2020 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package base
+
+import org.scalatest.OptionValues
+import org.scalatest.concurrent.IntegrationPatience
+import org.scalatest.concurrent.ScalaFutures
+import org.scalatest.freespec.AnyFreeSpec
+import org.scalatest.matchers.must.Matchers
+import play.api.inject.guice.GuiceApplicationBuilder
+
+trait SpecBase extends AnyFreeSpec with Matchers with OptionValues with ScalaFutures with IntegrationPatience {
+
+  type AppFunction = GuiceApplicationBuilder => GuiceApplicationBuilder
+
+  val baseApplicationBuilder: AppFunction = identity
+
+}
