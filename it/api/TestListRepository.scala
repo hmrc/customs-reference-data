3d39019494832fe904ce9de7b8bdf557f161149a
Jack Heslop <43777106+jackheslop96@users.noreply.github.com>
2022-02-22 19:53:04 +0000

    CTCTRADERS-2934: Integration test fixed.

--- it/api/TestListRepository.scala
+++ it/api/TestListRepository.scala
@@ -11,8 +11,6 @@ import scala.concurrent.Future
 class TestListRepository @Inject()(defaultListRepository: DefaultListRepository) extends ListRepository {
   override def getListByName(listNameDetails: VersionedListName): Future[Source[JsObject, Future[_]]] = defaultListRepository.getListByName(listNameDetails)
 
-  override def getListByName(listNameDetails: VersionedListName): Future[Seq[JsObject]] = defaultListRepository.getListByName(listNameDetails)
-
   override def getListNames(version: VersionId): Future[Seq[ListName]] = defaultListRepository.getListNames(version)
 
   override def insertList(list: Seq[GenericListItem]): Future[ListRepositoryWriteResult] = {
