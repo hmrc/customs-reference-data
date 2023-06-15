22cf96065bb8bd7bd72575619b8037cad9b9a1f7
ajchilts <40767309+ajchilts@users.noreply.github.com>
2023-06-15 11:56:31 +0100

    CTCP-3185 | Added filter params case class and query string binder

---
+++ app/models/FilterParams.scala
@@ -0,0 +1,36 @@
+/*
+ * Copyright 2023 HM Revenue & Customs
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package models
+
+import play.api.mvc.QueryStringBindable
+
+case class FilterParams(filters: Seq[(String, String)]) {}
+
+object FilterParams {
+
+  implicit def queryStringBindable: QueryStringBindable[FilterParams] =
+    new QueryStringBindable[FilterParams] {
+
+      override def bind(key: String, params: Map[String, Seq[String]]): Option[Either[String, FilterParams]] =
+        if (params.isEmpty)
+          Some(Left(""))
+        else
+          Some(Right(FilterParams(Seq("" -> "", "" -> ""))))
+
+      override def unbind(key: String, filters: FilterParams) = s"$key=$filters}"
+    }
+}
